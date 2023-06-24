cde1abf11bf7ac60c3cebcf7395670ec42796f99
calebhd03 <calebhd03@gmail.com>
2023-06-23 22:53:58 -0400

    imported text mesh pro

--- Assets/Claw.cs
+++ Assets/Claw.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Claw : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("Enemy"))
+        {
+            //Enemy Takes Damage
+        }
     }
 }
